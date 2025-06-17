77ad94334bde9d16808d300b2ef6caba7856d043
manriqueheidy <167436294+manriqueheidy@users.noreply.github.com>
2025-06-16 21:09:29 -0500

    Add files via upload

---
+++ basico/operaciones.py
@@ -0,0 +1,19 @@
+a = 10
+b = 4
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b  # división normal (decimal)
+division_entera = a // b  # solo la parte entera
+modulo = a % b  # residuo
+potencia = a ** b  # a elevado a la b
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("División entera:", division_entera)
+print("Módulo:", modulo)
+print("Potencia:", potencia)
+
